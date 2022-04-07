aa583bddb9612331d49267a34f06fa9aaf96d338
Gavin <gavin.dunnett@gmail.com>
2022-04-07 08:51:57 -0500

    fixed segmentation fault and added 1 card swap

--- 3cardmonte.c
+++ 3cardmonte.c
@@ -4,9 +4,12 @@
 
 int main()
 {
-	char* cards = "JQK";
+	char temp = ' ';
+	char cards[] = "JQK";
 	char a_card = cards[2];
+	temp = cards[2];
 	cards[2] = cards[1];
+	cards[1] = temp;
 	puts(cards);
 	return 0;
 }
